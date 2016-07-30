f1ad506c05ec1638d449ceb3c54ddb7c5202b440
OatMS <oat.boonnavasin@gmail.com>
2016-07-30 13:32:59 +0700

    After Presentation FinalProject and change Computer

--- sample/editGenogram.php
+++ sample/editGenogram.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <?php
   $altimgattr = array(
+"โรคความดันโลหิตสูง",
+"โรคซึมเศร้า",
+"โรคตับอักเสบ",
+"โรคติดต่อทางเพศสัมพันธ์",
+"โรคมะเร็ง",
+"โรคหัวใจ",
+"โรคออทิสติก",
+"โรคอัลไซเมอร์",
+"โรคอ้วน",
+"โรคเบาหวาน",
+"โรคเอดส์ - HIV",
+"โรคไขข้อ"
+);
+
+$altMentel = array(
 "ติดการพนัน",
 "ติดสารเสพติด",
 "ติดแอลกอฮอล์",
@@ -12,22 +27,7 @@
 "สงสัยว่ามาการดื่นแอลกอฮอล์และสารเสพติด",
 "อยู่ในระหว่างการรักษาอาการติดแอลกอฮอล์ หรือสารเสพติด",
 "อยู่ในระหว่างการรักษาอาการทางจิต และรักษาอาการติดแอลกอฮอล์หรือสารเสพติด",
-"อยู่ในระหว่างการรักษาอาการป่วยทางจิต",
-"โรคความดันโลหิตสูง",
-"โรคซึมเศร้า",
-"โรคตับอักเสบ",
-"โรคติดต่อทางเพศสัมพันธ์",
-"โรคมะเร็ง",
-"โรคหัวใจ",
-"โรคออทิสติก",
-"โรคอัลไซเมอร์",
-"โรคอ้วน.png",
-"โรคเบาหวาน",
-"โรคเอดส์ - HIV",
-"โรคไขข้อ"
-);
-
-
+"อยู่ในระหว่างการรักษาอาการป่วยทางจิต");
 
 $altimgline = array(
     "ความสัมพันธ์ที่ดี",
@@ -116,8 +116,15 @@ else{
     </div>
 
     <div id="container">
+        
 
         <div id="elementTools">
+          
+           <div id="searchDiv" >
+            <input type="text" id="keyword" name="keyword" >
+            <img id="searchIcon" src="img/searchIcon.png" onclick="search()">
+        </div>
+           
             <div id='cssmenu'>
                 <ul>
 
@@ -128,7 +135,7 @@ else{
                $i=0;
         for($i=0;$i<4;$i++ ){
        echo    "<a style='display:inline' title='".$altimgattr[$i]."' >";
-           echo "<img id='attrimg'   src='img/node/";
+           echo "<img class='attrimg'   src='img/node/";
         echo    $i+1;
         echo    ".png' /> ";
 
@@ -145,10 +152,10 @@ else{
                            <div style="text-align:center" >
      <?
                $i=0;
-        for($i=0;$i<24;$i++ ){
+        for($i=0;$i<12;$i++ ){
        echo    "<a style='display:inline' title='".$altimgattr[$i]."' >";
-           echo "<img id='attrimg'   src='img/attr/";
-        echo    $i+1;
+           echo "<img class='attrimg'   src='img/attr/";
+        echo    $i+13;
         echo    ".png' /> ";
 
          echo "  </a>";
@@ -167,7 +174,7 @@ else{
         for($i=0;$i<34;$i++ ){
 
             echo    "<a style='display:inline' title='".$altimgline[$i]."' >";
-           echo "<img id='attrimg'   src='img/line/";
+           echo "<img class='attrimg'   src='img/line/";
         echo    $i+1;
         echo    ".png' /> ";
 
@@ -180,15 +187,7 @@ else{
                         </ul>
                     </li>
 
-                    <li class='has-sub'><a href='#'>ค้นหา</a>
-                        <ul>
-                           <div style="text-align:center" >
-                           <input type="text" id="keyword" name="keyword" >
-                           <input type="button" onclick="search()" value="ค้นหา" >
-                           <input type="button" onclick="testHighlight()" value="Highlight" >
-                        </div>
-                        </ul>
-                    </li>
+                    
 
                 </ul>
             </div>
@@ -222,7 +221,7 @@ else{
 
 <!-- Button Openfilt -->
 
-        <div style="width:60%;
+        <div style="width:55%;
     height:650px; 
     float:left;position:relative; background-color:white;" >
             <a id="download-txt" download="genogram-info.txt">
@@ -335,23 +334,19 @@ else{
 
 </body>
 <!-- ******************* Context Node *********** --> 
-<!-- sep -->
-<nav id="context-menu" class="context-menu">
-    <ul class="context-menu__items">
-      <li class="context-menu__item">
-        <a href="#" class="context-menu__link" data-action="View" onclick="diagramAddSon()"><i class="fa fa-eye"></i> Add Son</a>
-      </li>
-      <li class="context-menu__item">
-        <a href="#" class="context-menu__link" data-action="Edit" onclick="diagramAddDaughter()"><i class="fa fa-edit"></i> Add Daughter</a>
-      </li>
-      <li class="context-menu__item">
-        <a href="#" class="context-menu__link" data-action="Delete"  onclick="diagramDeleteNode()"><i class="fa fa-times"></i> Delete Node</a>
-      </li>
-      <li class="context-menu__item">
-        <a href="#" class="context-menu__link" data-action="Delete"  onclick="diagramUndo()"><i class="fa fa-times"></i> Undo</a>
-      </li>
-    </ul>
-  </nav>
+  <div id="popup">
+      <div id="inPopup">
+          ชื่อ : <input name="n" id="n" type="text"><br>
+          อายุ : <input name="age"id="age" type="text"><br>
+          เพศ : <input name="s" id="s" type="text"><br>
+          โรคประจำตัว : <input name="a"id="a" type="text"><br>
+          คู่สมรส : <input name="cou"id="cou" type="text"><br>
+          <input type="button" onclick="updateEdit()" value="ตกลง">
+          <input type="button" onclick="cancleEdit()" value="ยกเลิก" >
+      </div>
+  </div>
+
+  
 </html>
 
 
@@ -440,240 +435,10 @@ return vars;
         createDi(userdata);
     }
 
-
-
+    
+    
 
     //***************Context JS********************
-
-(function() {
-  
-  "use strict";
-
-  //////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////
-  //
-  // H E L P E R    F U N C T I O N S
-  //
-  //////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////
-
-  /**
-   * Function to check if we clicked inside an element with a particular class
-   * name.
-   * 
-   * @param {Object} e The event
-   * @param {String} className The class name to check against
-   * @return {Boolean}
-   */
-  function clickInsideElement( e, className ) {
-    var el = e.srcElement || e.target;
-    
-    if ( el.classList.contains(className) ) {
-      return el;
-    } else {
-      while ( el = el.parentNode ) {
-        if ( el.classList && el.classList.contains(className) ) {
-          return el;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  /**
-   * Get's exact position of event.
-   * 
-   * @param {Object} e The event passed in
-   * @return {Object} Returns the x and y position
-   */
-  function getPosition(e) {
-    var posx = 0;
-    var posy = 0;
-
-    if (!e) var e = window.event;
-    
-    if (e.pageX || e.pageY) {
-      posx = e.pageX;
-      posy = e.pageY;
-    } else if (e.clientX || e.clientY) {
-      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-    }
-
-    return {
-      x: posx,
-      y: posy
-    }
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////
-  //
-  // C O R E    F U N C T I O N S
-  //
-  //////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////
-  
-  /**
-   * Variables.
-   */
-  var contextMenuClassName = "context-menu";
-  var contextMenuItemClassName = "context-menu__item";
-  var contextMenuLinkClassName = "context-menu__link";
-  var contextMenuActive = "context-menu--active";
-
-  var taskItemClassName = "task";
-  var taskItemInContext;
-
-  var clickCoords;
-  var clickCoordsX;
-  var clickCoordsY;
-
-  var menu = document.querySelector("#context-menu");
-  var menuItems = menu.querySelectorAll(".context-menu__item");
-  var menuState = 0;
-  var menuWidth;
-  var menuHeight;
-  var menuPosition;
-  var menuPositionX;
-  var menuPositionY;
-
-  var windowWidth;
-  var windowHeight;
-
-  /**
-   * Initialise our application's code.
-   */
-  function init() {
-    contextListener();
-    clickListener();
-    keyupListener();
-    resizeListener();
-  }
-
-  /**
-   * Listens for contextmenu events.
-   */
-  function contextListener() {
-    document.addEventListener( "contextmenu", function(e) {
-      taskItemInContext = clickInsideElement( e, taskItemClassName );
-
-      if ( taskItemInContext ) {
-        e.preventDefault();
-        toggleMenuOn();
-        positionMenu(e);
-      } else {
-        taskItemInContext = null;
-        toggleMenuOff();
-      }
-    });
-  }
-
-  /**
-   * Listens for click events.
-   */
-  function clickListener() {
-    document.addEventListener( "click", function(e) {
-      var clickeElIsLink = clickInsideElement( e, contextMenuLinkClassName );
-
-      if ( clickeElIsLink ) {
-        e.preventDefault();
-        menuItemListener( clickeElIsLink );
-      } else {
-        var button = e.which || e.button;
-        if ( button === 1 ) {
-          toggleMenuOff();
-        }
-      }
-    });
-  }
-
-  /**
-   * Listens for keyup events.
-   */
-  function keyupListener() {
-    window.onkeyup = function(e) {
-      if ( e.keyCode === 27 ) {
-        toggleMenuOff();
-      }
-    }
-  }
-
-  /**
-   * Window resize event listener
-   */
-  function resizeListener() {
-    window.onresize = function(e) {
-      toggleMenuOff();
-    };
-  }
-
-  /**
-   * Turns the custom context menu on.
-   */
-  function toggleMenuOn() {
-    if ( menuState !== 1 ) {
-      menuState = 1;
-      menu.classList.add( contextMenuActive );
-    }
-  }
-
-  /**
-   * Turns the custom context menu off.
-   */
-  function toggleMenuOff() {
-    if ( menuState !== 0 ) {
-      menuState = 0;
-      menu.classList.remove( contextMenuActive );
-    }
-  }
-
-  /**
-   * Positions the menu properly.
-   * 
-   * @param {Object} e The event
-   */
-  function positionMenu(e) {
-    clickCoords = getPosition(e);
-    clickCoordsX = clickCoords.x;
-    clickCoordsY = clickCoords.y;
-
-    menuWidth = menu.offsetWidth + 4;
-    menuHeight = menu.offsetHeight + 4;
-
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
-
-    if ( (windowWidth - clickCoordsX) < menuWidth ) {
-      menu.style.left = windowWidth - menuWidth + "px";
-    } else {
-      menu.style.left = clickCoordsX + "px";
-    }
-
-    if ( (windowHeight - clickCoordsY) < menuHeight ) {
-      menu.style.top = windowHeight - menuHeight + "px";
-    } else {
-      menu.style.top = clickCoordsY + "px";
-    }
-  }
-
-  /**
-   * Dummy action function that logs an action when a menu item link is clicked
-   * 
-   * @param {HTMLElement} link The link that was clicked
-   */
-  function menuItemListener( link ) {
-    console.log( "Task ID - " + taskItemInContext.getAttribute("data-id") + ", Task action - " + link.getAttribute("data-action"));
-    toggleMenuOff();
-  }
-
-  /**
-   * Run the app.
-   */
-  init();
-
-})();
 
 //----------------------------
 
